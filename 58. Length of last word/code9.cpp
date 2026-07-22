599a1572483de50690cfc75b21db045fb4661cf9
sanskrutidahiphale <sanskruti.dahiphale23@pccoepune.org>
2026-07-22 12:59:32 +0530

    Added last word solution

---
+++ 58. Length of last word/code9.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int i = s.length() - 1;
+
+        // Skip trailing spaces
+        while (i >= 0 && s[i] == ' ')
+            i--;
+
+        int count = 0;
+
+        // Count characters of last word
+        while (i >= 0 && s[i] != ' ') {
+            count++;
+            i--;
+        }
+
+        return count;
+    }
+};
